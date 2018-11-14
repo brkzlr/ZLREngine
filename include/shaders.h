4bfdfe074d36a3a3fa0d0c024628e978c0b64e76
brkzlr <brkozler@gmail.com>
2018-11-14 15:19:45 +0200

    Added shader class

---
+++ include/shaders.h
@@ -0,0 +1,33 @@
+#ifndef SHADERS_H
+#define SHADERS_H
+
+#include <glad.h>
+#include <glm/glm.hpp>
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+class Shaders{
+private:
+	unsigned int ID;
+	void checkCompileErrors(GLuint shader, std::string type);
+
+public:
+	//We'll leave geometryPath as null as we do not use geometry shaders for now.
+	Shaders(const char* vertexPath, const char* fragmentPath, const char* geometryPath);
+	void use();
+
+	//Shader uniform setting functions
+	void setBool(const std::string &name, bool value) const;
+	void setInt(const std::string &name, int value) const;
+	void setFloat(const std::string &name, float value) const;
+	void setVec2(const std::string &name, const glm::vec2 &value) const;
+	void setVec3(const std::string &name, const glm::vec3 &value) const;
+	void setVec4(const std::string &name, const glm::vec4 &value) const;
+	void setMat2(const std::string &name, const glm::mat2 &mat) const;
+	void setMat3(const std::string &name, const glm::mat3 &mat) const;
+	void setMat4(const std::string &name, const glm::mat4 &mat) const;
+};
+#endif
