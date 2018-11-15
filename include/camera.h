b1389c56ab451c91c3e9aeae9b474362851261df
brkzlr <brkozler@gmail.com>
2018-11-15 02:17:41 +0200

    Add camera class

---
+++ include/camera.h
@@ -0,0 +1,39 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include <glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+class Camera{
+public:
+	enum Camera_Direction{
+		FORWARD,
+		BACKWARD,
+		LEFT,
+		RIGHT
+	};
+
+private:
+	glm::vec3 Position;
+	glm::vec3 Front;
+	glm::vec3 Up;
+	glm::vec3 Right;
+	glm::vec3 WorldUp;
+
+	float Yaw;
+	float Pitch;
+
+	float MovementSpeed;
+	float MouseSens;
+
+	void updateCameraVectors();
+
+public:
+	Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = -90.0f, float pitch = 0.0f);
+	glm::mat4 GetViewMatrix();
+	void MoveCamera(Camera_Direction direction, float deltaTime);
+	void CameraLook(float xPos, float yPos);
+};
+
+#endif
