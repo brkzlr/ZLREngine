059c2f138d0a23fe931e10527bd0273c3dfe72ed
brkzlr <brkozler@gmail.com>
2022-09-29 00:35:57 +0300

    Add Input Manager

--- src/Events/EventDispatcher.h
+++ src/Events/EventDispatcher.h
@@ -9,34 +9,15 @@ class EventDispatcher{
 
 public:
 	static EventDispatcher* GetInstance();
+	EventDispatcher(const EventDispatcher&) = delete;
+	void operator=(const EventDispatcher&) = delete;
 
  	template<typename T, typename EventType>
-	void AddListener(T* classInstance, void (T::*memberFunc)(EventType*)){
-		ListenerList* listeners = m_ListenersMap[typeid(EventType)];
+	void AddListener(T* classInstance, void (T::*memberFunc)(EventType*));
 
-		if(!listeners){
-			listeners = new ListenerList();
-			m_ListenersMap[typeid(EventType)] = listeners;
-		}
-
-		listeners->push_back(new EventListener<EventType>(std::bind(memberFunc, classInstance, std::placeholders::_1)));
-	}
-
-	//TO DO: Make an event queue and process it at end of frame
+	//@TODO: Make an event queue and process it at end of frame
 	template<typename EventType>
-	void Broadcast(EventType* event){
-		ListenerList* listeners = m_ListenersMap[typeid(EventType)];
-
-		if (!listeners){
-			return;
-		}
-
-		for (EventListenerBase* listener : *listeners){
-			if (listener){
-				listener->Call(event);
-			}
-		}
-	}
+	void Broadcast(EventType* event);
 
 private:
 	static EventDispatcher* ms_Instance;
@@ -44,5 +25,34 @@ private:
 
 	std::map<std::type_index, ListenerList*> m_ListenersMap;
 };
+
+////////////////////////////////////////////////////////////
+////////////////////// Templates impl //////////////////////
+template<typename T, typename EventType>
+void EventDispatcher::AddListener(T* classInstance, void (T::*memberFunc)(EventType*)){
+	ListenerList* listeners = m_ListenersMap[typeid(EventType)];
+
+	if(!listeners){
+		listeners = new ListenerList();
+		m_ListenersMap[typeid(EventType)] = listeners;
+	}
+
+	listeners->push_back(new EventListener<EventType>(std::bind(memberFunc, classInstance, std::placeholders::_1)));
+}
+
+template<typename EventType>
+void EventDispatcher::Broadcast(EventType* event){
+	ListenerList* listeners = m_ListenersMap[typeid(EventType)];
+
+	if (!listeners){
+		return;
+	}
+
+	for (EventListenerBase* listener : *listeners){
+		if (listener){
+			listener->Call(event);
+		}
+	}
+}
 
 #endif
